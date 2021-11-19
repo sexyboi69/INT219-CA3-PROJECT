101e8fe412daf83424700c3decb3718df406a774
sexyboi69 <63389499+sexyboi69@users.noreply.github.com>
2021-11-19 10:01:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const background = () => {
+  const randCol = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randCol;
+  hex_code.innerHTML = "HEX-COLOR: " + "#" + randCol;
+};
